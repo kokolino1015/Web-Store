1136851fee30759c228170ae558a1cce574d4b65
Kaloyan Nedelkov <71516863+kokolino1015@users.noreply.github.com>
2024-04-06 12:35:20 +0300

    Delete Models directory

--- Models/CategoryModel/CategoryFormModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebStore.Models.CategoryModel
-{
-    public class CategoryFormModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebStore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
